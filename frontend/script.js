224ee83db17cee2c3522f0b20ccaded25e510dfe
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-04-25 13:16:12 +0200

    04.25. monday

---
+++ frontend/script.js
@@ -0,0 +1,31 @@
+const firstPerson = {
+    name: "Peter",
+    sex: "male",
+}
+
+const secondPerson = {
+    name: "Sarah",
+    sex: "female"
+}
+
+function detectSex(person) {
+    console.log(person)
+
+    if (person.sex === "female") {
+        return "This person is a female."
+    } else if (person.sex === "male") {
+        return "This person is a male"
+    }
+    else {
+        return "the person's sex is not female nor male"
+    }
+}
+
+console.log(detectSex(firstPerson));
+console.log(detectSex(secondPerson));
+
+console.log(detectSex({
+    name: "Penelope",
+    sex: "female2"
+}
+));
